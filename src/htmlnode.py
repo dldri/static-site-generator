4b7320a627e82663ccf52a487c08a5d7d03e1094
dldri <diovian.liu@gmail.com>
2025-02-01 05:28:47 +0800

    added leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,11 +9,24 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
+        if not self.props:
+            return ""
         string = ""
         for key, value in self.props.items():
-            string += f'{key}="{value}" '
+            string += f' {key}="{value}"'
         
-        return string.rstrip()
+        return string
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props_to_html})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, props=props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Leaf Node MUST have a value.")
+        if not self.tag:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -8,9 +8,24 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("p", "content", props={"href": "https://boot.dev"})
         node2 = HTMLNode("p", "content", props={"src": "https://image.png"})
         node3 = HTMLNode("a", "value", node, {"href": "test", "a": "another test", "third_prop": "third"})
+        node4 = HTMLNode(value="test")
         print(node.props_to_html())
         print(node2.props_to_html())
         print(node3.props_to_html())
+        print(node4.props_to_html())
+
+class TestLeafNode(unittest.TestCase):
+    def test_eq(self):
+        leafnode1 = LeafNode("p", "this is a paragraph")
+        leafnode2 = LeafNode("a", "link", {"href": "https://boot.dev"})
+        leafnode3 = LeafNode("img", "image", {"src": "/dog.png"})
+        leafnode4 = LeafNode(None, "raw text")
+        
+        print(leafnode1.to_html())
+        print(leafnode2.to_html())
+        print(leafnode3.to_html())
+        print(leafnode4.to_html())
+        
 
 
 if __name__ == "__main__":
